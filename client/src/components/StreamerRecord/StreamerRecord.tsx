968568b38e94004023e9d34f0ea7ee38b694d8e3
ifmcjthenknczny <maciej.konieczny.1993@gmail.com>
2023-07-02 16:47:03 +0200

    centered streamerRecord, white heading

--- client/src/components/StreamerRecord/StreamerRecord.tsx
+++ client/src/components/StreamerRecord/StreamerRecord.tsx
@@ -1,23 +1,23 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './StreamerRecord.module.scss'
-import {useParams} from 'react-router-dom'
-import {IMAGE_URL} from '../../constants'
+import { useParams } from 'react-router-dom'
+import { IMAGE_URL } from '../../constants'
 import Error from '../ErrorPage/ErrorPage'
-import {query} from '../../helpers'
+import { query } from '../../helpers'
 import useBusy from '../../hooks/useBusy'
 import Spinner from '../Spinner/Spinner'
 import GoBackButton from '../GoBackButton/GoBackButton'
 import Heading from '../Heading/Heading'
-import {type PublicStreamer} from '../../contract'
+import { type PublicStreamer } from '../../contract'
 
 const StreamerRecord = () => {
-    const {streamerId} = useParams()
+    const { streamerId } = useParams()
     const [streamer, setStreamer] = useState<PublicStreamer>()
     const [error, setError] = useState(false)
     const [isBusy, busyWrapper] = useBusy(true)
 
     const fetchStreamer = busyWrapper(async (streamerId: string) => {
-        const streamer = await query('/streamer/:streamerId', {params: {streamerId}})
+        const streamer = await query('/streamer/:streamerId', { params: { streamerId } })
         setStreamer(streamer)
     })
 
@@ -37,18 +37,19 @@ const StreamerRecord = () => {
         return <Error />
     }
 
-    return <><div className={styles.container}>
-        <Heading className={styles.name} title={streamer.name} />
-        <div className={styles.info}>
-            <img className={styles.image} src={IMAGE_URL} alt={streamer.name} />
-            <div className={styles.details}>
-                <p className={styles.description}>{streamer.description}</p>
-                <p className={styles.platform}>Platform: <strong>{streamer.platform}</strong></p>
+    return <div className={styles.wrapper}>
+        <div className={styles.container}>
+            <Heading className={styles.name} title={streamer.name} />
+            <div className={styles.info}>
+                <img className={styles.image} src={IMAGE_URL} alt={streamer.name} />
+                <div className={styles.details}>
+                    <p className={styles.description}>{streamer.description}</p>
+                    <p className={styles.platform}>Platform: <strong>{streamer.platform}</strong></p>
+                </div>
             </div>
         </div>
+        <GoBackButton />
     </div>
-    <GoBackButton />
-    </>
 }
 
 export default StreamerRecord
